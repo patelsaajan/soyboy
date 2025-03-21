2ad0868f53ddc27f42dd73d480419aa873e31ddb
Saajan Patel <saajan.patel@hoppstudio.com>
2025-03-21 18:07:32 +0000

    feat: bringing work over from last project

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>
-        <h1 class="text-primary text-heading">Home Page</h1>
+    <div class="container">
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 h-full py-8">
+            <PartsRecipeCard 
+                v-for="n in 12" 
+                :key="n" 
+                :recipe="todaysRecipe"
+            />
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import type IRecipes from '~~/types/recipes';
+
+
+const todaysRecipe : IRecipes = {
+        name: 'Korean Satay Sauce Tofu',
+        cuisine: 'Korean',
+        description: 'This is a delicious recipe that will make your day special This is a delicious recipe that will make your day special This is a delicious recipe that will make your day special This is a delicious recipe that will make your day special',
+        image: '/imgs/default-food.jpg',
+        difficulty: 'easy',
+        preparationTime: 30,
+        servings: 4,
+};
 
 </script>
 
